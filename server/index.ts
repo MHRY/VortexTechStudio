c7e63555d5d2f19250f95257040f63763065a676
Muhammad Hamza Rauf <14033711+MuhammadHamzaRauf@users.noreply.github.com>
2025-07-31 13:32:56 +0500

    Making port changes

--- server/index.ts
+++ server/index.ts
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
   });
 
   const port = 5000;
-  const host = "127.0.0.1";
+  const host = "0.0.0.0";
   const server = http.createServer(app);
 
   if (app.get("env") === "development") {
